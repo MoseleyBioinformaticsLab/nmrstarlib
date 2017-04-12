70271ac62fe090259360f37182aaf7f049249dd2
Andrey Smelter <smelandr@gmail.com>
2017-04-12 00:49:07 -0400

    Parametrized csviwer testing code.

--- tests/test_csviewer.py
+++ tests/test_csviewer.py
@@ -1,5 +1,7 @@
-import shutil
 import os
+import shutil
+import pytest
+
 from nmrstarlib.csviewer import CSViewer
 
 
@@ -10,55 +12,24 @@ def teardown_module(module):
         shutil.rmtree("tests/example_data/NMRSTAR2/tmp")
 
 
-def test_csviewer_all():
+@pytest.mark.parametrize("amino_acids,atoms", [
+    (None, None),
+    (["SER", "MET"], None),
+    (["SER", "MET"], ["CA", "CB"])
+])
+def test_csviewer(amino_acids, atoms):
     csviewer = CSViewer(from_path="tests/example_data/NMRSTAR3/bmr18569.str",
-                        amino_acids=(),
-                        atoms=(),
+                        amino_acids=amino_acids,
+                        atoms=atoms,
                         filename="tests/example_data/NMRSTAR3/tmp/18569",
                         csview_format="png",
                         nmrstar_version="3")
     csviewer.csview(view=False)
 
     csviewer = CSViewer(from_path="tests/example_data/NMRSTAR2/bmr18569.str",
-                        amino_acids=(),
-                        atoms=(),
+                        amino_acids=amino_acids,
+                        atoms=atoms,
                         filename="tests/example_data/NMRSTAR2/tmp/18569",
-                        csview_format="png",
-                        nmrstar_version="2")
-    csviewer.csview(view=False)
-
-
-def test_csviewer_specific_amino_acid():
-    csviewer = CSViewer(from_path="tests/example_data/NMRSTAR3/bmr18569.str",
-                        amino_acids=("SER",),
-                        atoms=(),
-                        filename="tests/example_data/NMRSTAR3/tmp/18569_SER",
-                        csview_format="png",
-                        nmrstar_version="3")
-    csviewer.csview(view=False)
-
-    csviewer = CSViewer(from_path="tests/example_data/NMRSTAR2/bmr18569.str",
-                        amino_acids=("SER",),
-                        atoms=(),
-                        filename="tests/example_data/NMRSTAR2/tmp/18569_SER",
-                        csview_format="png",
-                        nmrstar_version="2")
-    csviewer.csview(view=False)
-
-
-def test_csviewer_specific_amino_acid_specific_atom():
-    csviewer = CSViewer(from_path="tests/example_data/NMRSTAR3/bmr18569.str",
-                        amino_acids=("SER",),
-                        atoms=("CA", "CB"),
-                        filename="tests/example_data/NMRSTAR3/tmp/18569_SER_CA_CB",
-                        csview_format="png",
-                        nmrstar_version="3")
-    csviewer.csview(view=False)
-
-    csviewer = CSViewer(from_path="tests/example_data/NMRSTAR2/bmr18569.str",
-                        amino_acids=("SER",),
-                        atoms=("CA", "CB"),
-                        filename="tests/example_data/NMRSTAR2/tmp/18569_SER_CA_CB",
                         csview_format="png",
                         nmrstar_version="2")
     csviewer.csview(view=False)
