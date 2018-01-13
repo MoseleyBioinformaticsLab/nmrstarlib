6b80fc283ae9c0cacb12b93375c283d4b826db35
Andrey Smelter <andrey.smelter@gmail.com>
2018-01-12 20:00:07 -0500

    Bump up nmrstarlib version.

--- nmrstarlib/__init__.py
+++ nmrstarlib/__init__.py
@@ -1,42 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Routines for working with BMRB NMR-STAR format files.
+"""Routines for working with ``BMRB NMR-STAR`` and ``PDB CIF`` formatted files.
 
 This package includes the following modules:
 
 ``nmrstarlib``
-    This module provides the :class:`~nmrstarlib.nmrstarlib.StarFile` class which is a python
-    dictionary representation of a BMRB NMR-STAR file. Data can be accessed
-    directly from the :class:`~nmrstarlib.nmrstarlib.StarFile` instance using bracket accessors.
+    This module provides the :class:`~nmrstarlib.nmrstarlib.StarFile` superclass and 
+    :class:`~nmrstarlib.nmrstarlib.NMRStarFile` and :class:`~nmrstarlib.nmrstarlib.CIFFile`
+    which are python dictionary representation of a BMRB NMR-STAR file and PDB CIF file, 
+    respectively. Data can be accessed directly from the instance using bracket accessors.
     The :mod:`~nmrstarlib.nmrstarlib` module relies on the :mod:`~nmrstarlib.bmrblex` module
     for processing of tokens.
 
 ``bmrblex``
     This module provides the :func:`~nmrstarlib.bmrblex.bmrblex` generator that is responsible
-    for the syntax analysis of BMRB NMR-STAR files, processing word, number, single quoted,
-    double quoted, multiline quoted BMRB tokens.
+    for the syntax analysis of BMRB NMR-STAR and PDB CIF files, processing word, number, single quoted,
+    double quoted, multiline quoted tokens.
 
 ``converter``
     This module provides the :class:`~nmrstarlib.converter.Converter` class that is
-    responsible for the conversion of NMR-STAR formated files.
+    responsible for the conversion of NMR-STAR and CIF formatted files.
 
 ``csviewer``
     This module provides the :class:`~nmrstarlib.csviewer.CSViewer` class that visualizes
-    chemical shift values using the Graphviz (http://www.graphviz.org/) DOT Languge description
-    and provides code example for utilizing the library.
+    chemical shift values from NMR-STAR files using the Graphviz (http://www.graphviz.org/) 
+    DOT Languge description and provides code example for utilizing the library.
 
 ``noise``
     This module provides the :class:`~nmrstarlib.noise.NoiseGenerator` class 
-    for adding random normal noise values to peaks in simulated peak list.
+    for adding random normal noise values to peaks in simulated peak list for 
+    NMR-STAR formatted files.
 
 ``plsimulator``
     This module provides necessary interfaces in order to create a simulated
-    :class:`~nmrstarlib.plsimulator.PeakList`.
+    :class:`~nmrstarlib.plsimulator.PeakList` from NMR-STAR formatted files.
 
 ``translator``
     This module provides :class:`~nmrstarlib.translator.StarFileToStarFile` for
-    conversion between NMR-STAR and JSONized NMR-STAR formatted files and
+    conversion between NMR-STAR/CIF and JSONized NMR-STAR/CIF formatted files and
     :class:`~nmrstarlib.translator.StarFileToPeakList` for conversion of NMR-STAR
     formatted files into peak list files using chemical shift values and assignment
     information.
@@ -47,7 +49,7 @@ This package includes the following modules:
     machine, directory/archive of files, URL address of a file).
 """
 
-__version__ = "2.0.6"
+__version__ = "2.1.0"
 
 
 from .fileio import read_files
