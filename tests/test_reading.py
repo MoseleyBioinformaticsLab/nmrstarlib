bd04bcbbefaf90ef5fab6dd6bab9a9b7f6f2d1fd
Andrey Smelter <andrey.smelter@gmail.com>
2018-05-03 17:39:12 -0400

    Added additional test to read from URL.

--- tests/test_reading.py
+++ tests/test_reading.py
@@ -26,6 +26,17 @@ def test_from_bmrbid(source):
 
 
 @pytest.mark.parametrize("source", [
+    ("http://rest.bmrb.wisc.edu/bmrb/NMR-STAR3/15000",
+     "http://rest.bmrb.wisc.edu/bmrb/NMR-STAR3/18569")
+])
+def test_from_url(source):
+    starfile_generator = nmrstarlib.read_files(*source)
+    starfile1 = next(starfile_generator)
+    starfile2 = next(starfile_generator)
+    assert starfile1.id in ("15000", "18569") and starfile2.id in ("15000", "18569")
+
+
+@pytest.mark.parametrize("source", [
     "tests/example_data/NMRSTAR3/starfiles_directory",
     "tests/example_data/NMRSTAR2/starfiles_directory",
     "tests/example_data/CIF/ciffiles_directory",
